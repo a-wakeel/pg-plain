760137089bf09bfc002a0dc39316a9e0e721001c
awakeel <awkhan978@gmail.com>
2018-03-06 16:33:13 +0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pg-plain',
+    version='1.0.0',
+    packages=['pg_plain'],
+    url='https://github.com/a-wakeel/pg-plain',
+    license='MIT',
+    author='awakeel',
+    author_email='awkhan978@gmail.com',
+    description='A plain wrapper around psycopg2'
+)
