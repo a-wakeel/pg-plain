367a11a39e9ff4011250517ea5bf62872b36d028
a-wakeel <awkhan978@gmail.com>
2018-03-09 16:57:41 +0500

    add test skeltons

---
+++ tests/test_pg.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestPg(TestCase):
+    def test_connect(self):
+        self.fail()
+
+    def test_execute(self):
+        self.fail()
+
+    def test_commit(self):
+        self.fail()
+
+    def test_close(self):
+        self.fail()
---
+++ tests/test_pgPool.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestPgPool(TestCase):
+    def test_create_pool(self):
+        self.fail()
+
+    def test_get_conn(self):
+        self.fail()
+
+    def test_execute_query(self):
+        self.fail()
+
+    def test_commit_changes(self):
+        self.fail()
+
+    def test_put_conn(self):
+        self.fail()
+
+    def test_close_pool(self):
+        self.fail()
